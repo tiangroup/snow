71880533ac1881aeb62ac7e670c3a8bcd403fa49
pavlenkoia <pavlenkoia@gmail.com>
2024-12-19 16:00:06 +0500

    Update show.js

--- show.js
+++ show.js
@@ -1,10 +1,1 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdn.jsdelivr.net/gh/Alaev-Co/snowflakes/dist/snow.min.css';
-document.head.appendChild(link);
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/gh/Alaev-Co/snowflakes/dist/Snow.min.js';
-script.onload = () => {
-    new Snow();
-};
-document.body.appendChild(script);
+
